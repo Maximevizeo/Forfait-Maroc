56896d7fc5a40048e801c4c9b802f69bab9c7492
abou bichr <abou.bichr.services@laposte.net>
2017-10-05 19:31:12 +0200

    Fixed error test

--- src/main/java/org/sid/MonBondManagerApplication.java
+++ src/main/java/org/sid/MonBondManagerApplication.java
@@ -17,6 +17,6 @@ public class MonBondManagerApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		//Candidat c1=candidatRepository.save(new Candidat("ABOU ABBAS", "Cécile", "Paris", "06 42 60 00 39", "maher@vizeo-technologies.com", "stagiaire"));
 		//Candidat c2=candidatRepository.save(new Candidat("ZIDANE", "Zinedine", "Madrid", "06 55 56 54 54", "zidane@zizou.com", "A contacter"));
-		
+		// test
 	}
 }
